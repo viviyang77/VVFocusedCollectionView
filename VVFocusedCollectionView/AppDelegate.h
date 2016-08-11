932bd414a2d6b5831032ffb918bb24fa9c0752e9
Vivi <vivi@ddrift.com>
2016-08-11 15:39:45 +0800

    Initial Commit

---
+++ VVFocusedCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VVFocusedCollectionView
+//
+//  Created by Work on 8/11/16.
+//  Copyright © 2016 Work. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VVFocusedCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VVFocusedCollectionView
+//
+//  Created by Work on 8/11/16.
+//  Copyright © 2016 Work. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
